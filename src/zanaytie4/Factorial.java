6f4447398c0b3434ef7fdf609534f9f52b3acb70
Igor85342 <imoskalets@gmail.com>
2016-06-14 21:54:55 +0300

    add structure

---
+++ src/zanaytie4/Factorial.java
@@ -0,0 +1,26 @@
+package zanaytie4;
+
+
+public class Factorial{
+
+	
+
+	public static void main(String[] args){
+		
+		
+		int step = 10;
+		int value = 1;	
+		for (int i = 1; i < step +1 ; i++) {
+
+			value = value * i;	
+		}
+        
+		System.out.println(value);
+		
+		
+		
+		
+
+	}
+}
+
