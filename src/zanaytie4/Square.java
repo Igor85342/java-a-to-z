b84c295e2603c1f8c6b9571c6d98efcfd0024626
Igor85342 <imoskalets@gmail.com>
2016-06-18 19:01:45 +0300

    add structure

--- src/zanaytie4/Square.java
+++ src/zanaytie4/Square.java
@@ -4,45 +4,44 @@ import static java.lang.Math.*;
 public class Square{
 
 	
-	double x1;
-	double x2;
+	double a;
+	double b;
+	double c;
 
+	Square (double a, double b, double c) {
+
+	this.a = a ;
+	this.b = b ;
+	this.c = c ;
+
+	}
+	
+	
 	
 
 	public static void main(String[] args){
-		double a = 1;
-		double b = -5;
-		double c  = 3;
-		double step = Double.valueOf(args[0]);
-		Square square = new Square();	
-		square.floatCalculate(a,b,c,step);
-
-		for (double i = square.x1; i <= square.x2; i = i + step){
+		
+		double a = Double.valueOf(args[0]);
+		double b = Double.valueOf(args[1]);
+		double c = Double.valueOf(args[2]);
+		double x1 = Double.valueOf(args[3]);
+		double x2 = Double.valueOf(args[4]);
+		double step = Double.valueOf(args[5]);
+		
+		int x =4;
+ 
+		Square square = new Square(a,b,c);	
+		float y = square.calculate(x);
+		System.out.println(y);
+		for (double i = x1; i <= x2; i = i + step){
 				System.out.println(i);	
-			}
+		}
 		
 	}
 
-	public void floatCalculate(double a, double b, double c, double step){
-		double  d;
+	public float calculate(int x){
 		
-		System.out.println(a);	
-		d = Math.pow(b,2) - 4 * a * c;
-		
-		if ( d < 0 ) {
-			System.out.println("The quadratic equation has no solution.");	
-		}
-
-		if ( d == 0) {
-			System.out.println("One root");	
-		}
-		
-		if ( d > 0 ) {
-			this.x1 = (- b - sqrt(d))/ 2 * a;	
-			this.x2 = (- b + sqrt(d))/ 2 * a;	
-			
-			
-		}
+		return (float)(this.a * Math.pow(x,2) + this.b * x + this.c);
 		
 		
 
