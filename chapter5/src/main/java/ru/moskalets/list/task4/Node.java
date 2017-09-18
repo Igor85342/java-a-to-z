6a411038a2219bf91158c9a6c468e0ab01af51cf
Igor85342 <imoskalets@gmail.com>
2017-09-18 21:01:40 +0300

    Задан связанный список. Определить цикличность. [#161]. Update 18.09.2017.

--- chapter5/src/main/java/ru/moskalets/list/task4/Node.java
+++ chapter5/src/main/java/ru/moskalets/list/task4/Node.java
@@ -70,32 +70,4 @@ public class Node<T> {
     public T getValue() {
         return this.value;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Node<?> node = (Node<?>) o;
-
-        if (value != null ? !value.equals(node.value) : node.value != null) {
-            return false;
-        }
-        if (prev != null ? !prev.equals(node.prev) : node.prev != null) {
-            return false;
-        }
-        return next != null ? next.equals(node.next) : node.next == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = value != null ? value.hashCode() : 0;
-        result = 31 * result + (prev != null ? prev.hashCode() : 0);
-        result = 31 * result + (next != null ? next.hashCode() : 0);
-        return result;
-    }
 }
