ea07aff10b2b17b8d9f5b666daab247ce64d8101
Igor85342 <imoskalets@gmail.com>
2017-10-19 07:13:23 +0300

    Создать модель User [#999]. Add 19.10.2017.

---
+++ chapter5/src/main/java/ru/moskalets/map/task1/User.java
@@ -0,0 +1,21 @@
+package ru.moskalets.map.task1;
+
+import java.util.Calendar;
+
+/**
+ * This is the class user.
+ */
+public class User {
+    /**
+     * name.
+     */
+    private String name;
+    /**
+     * children.
+     */
+    private int children;
+    /**
+     * birthday.
+     */
+    private Calendar birthday;
+}
---
+++ chapter5/src/main/java/ru/moskalets/map/task1/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for map.task1.
+ *
+ * @author Igor Moskalets
+ * @version 1.0
+ * @since 19.10.2017
+ */
+package ru.moskalets.map.task1;
