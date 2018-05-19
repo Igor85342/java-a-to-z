9707cc7d6e252c6eef4cd1d543b86e52bea45db3
igor85342 <imoskalets@gmail.com>
2018-05-19 20:16:41 +0300

    Deadlock.Update 19.05.2018.

--- level2/src/main/java/ru/moskalets/chapter2/controlquestion/task002/Deadlock.java
+++ level2/src/main/java/ru/moskalets/chapter2/controlquestion/task002/Deadlock.java
@@ -4,8 +4,10 @@ package ru.moskalets.chapter2.controlquestion.task002;
  * В классе реализован гарантированный DeadLock.
  */
 public class Deadlock {
-    private Object lock001 = new Object();
-    private Object lock002 = new Object();
+    private final Thread mainThread;
+    public Deadlock() {
+        this.mainThread = Thread.currentThread();
+    }
     public static void main(String[] args) {
         Deadlock dl = new Deadlock();
         dl.init();
@@ -14,46 +16,19 @@ public class Deadlock {
         Thread thread001 = new Thread() {
             @Override
             public void run() {
-                synchronized (Deadlock.this.lock001) {
-                    try {
-                        sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    synchronized (Deadlock.this.lock002) {
-                        try {
-                            sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        lock002.notify();
-                    }
-                    lock001.notify();
+                try {
+                    Deadlock.this.mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-        };
-        Thread thread002 = new Thread() {
-            @Override
-            public void run() {
-                synchronized (Deadlock.this.lock002) {
-                    try {
-                        sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    synchronized (Deadlock.this.lock001) {
-                        try {
-                            sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        lock001.notify();
-                    }
-                    lock002.notify();
-                }
-            }
+
         };
         thread001.start();
-        thread002.start();
+        try {
+            thread001.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- level2/src/main/java/ru/moskalets/chapter2/controlquestion/task002/package-info.java
+++ level2/src/main/java/ru/moskalets/chapter2/controlquestion/task002/package-info.java
@@ -1,6 +1,6 @@
 /**
  * @author Igor Moskalets (imoskalets@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 16.05.2018
  */
 package ru.moskalets.chapter2.controlquestion.task002;
