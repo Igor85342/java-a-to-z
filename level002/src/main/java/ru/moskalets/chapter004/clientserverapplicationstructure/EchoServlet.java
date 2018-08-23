7f50b6755951aa0c8dd7782edabb79a5a487bcb5
igor85342 <imoskalets@gmail.com>
2018-08-23 07:30:04 +0300

    Создать web проект [#2514]. Add 23.08.2018.

---
+++ level002/src/main/java/ru/moskalets/chapter004/clientserverapplicationstructure/EchoServlet.java
@@ -0,0 +1,26 @@
+package ru.moskalets.chapter004.clientserverapplicationstructure;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class EchoServlet extends HttpServlet {
+    private static Logger log = LoggerFactory.getLogger(EchoServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("Hello world!");
+        writer.flush();
+    }
+}
